3105c15fe2eadc91a51f9b4970c9ab4c5ed1c0bc
jerome amos <mail@amosdesigns.net>
2021-07-19 19:10:16 -0400

    updated by breaking up homepage into components
    Signed-off-by: jerome amos <mail@amosdesigns.net>

--- src/components/menuitem/MenuItem.jsx
+++ src/components/menuitem/MenuItem.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import "./menu-item.styles.scss";
 
 const MenuItem = ({ title, imageUrl, size }) => (
-  <div
-    style={{ backgroundImage: `url(${imageUrl})` }}
-    className={size == "large" ? "menu-item large" : "menu-item"}
-  >
+  <div className={size === "large" ? "menu-item large" : "menu-item"}>
+    <div
+      style={{ backgroundImage: `url(${imageUrl})` }}
+      className="background-image"
+    ></div>
     <div className="content">
       <h1 className="title">{title}</h1>
       <span className="subtitle">SHOP NOW</span>
