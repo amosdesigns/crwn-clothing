f141983e4a273bdac29f20143b7e82059e998fb9
jerome amos <mail@amosdesigns.net>
2021-07-19 17:35:55 -0400

    leson 71
    Signed-off-by: jerome amos <mail@amosdesigns.net>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import HomePage from './Homepage.component';
+import HomePage from './pages/homepage/Homepage.component';
 import './App.css';
 
 function App() {
---
+++ src/components/menuitem/MenuItem.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./menu-item.styles.scss";
+
+const MenuItem = ({ title, imageUrl, size }) => (
+  <div
+    style={{ backgroundImage: `url(${imageUrl})` }}
+    className={size == "large" ? "menu-item large" : "menu-item"}
+  >
+    <div className="content">
+      <h1 className="title">{title}</h1>
+      <span className="subtitle">SHOP NOW</span>
+    </div>
+  </div>
+);
+export default MenuItem;
