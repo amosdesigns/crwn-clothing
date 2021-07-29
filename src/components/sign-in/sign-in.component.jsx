f379092d5025c48c177e14050cf2d121d0852b16
jerome amos <mail@amosdesigns.net>
2021-07-29 19:30:54 -0400

    updated the google signin button
    Signed-off-by: jerome amos <mail@amosdesigns.net>

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -56,7 +56,7 @@ class SignIn extends React.Component {
           />
           <div className="buttons">
             <CustomButton type="submit"> Sign In </CustomButton>
-            <CustomButton onClick={signInWithGoogle} isGoogleSignIn={true}>
+            <CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn={true}>
               {""}
               Sign In with Goggle
               {""}
