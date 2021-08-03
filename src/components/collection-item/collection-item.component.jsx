3317d09a31d862e02d919313eb9e3465b40f6983
jerome amos <mail@amosdesigns.net>
2021-08-03 16:07:08 -0400

    Added add to cart button to collection area for cart
    Signed-off-by: jerome amos <mail@amosdesigns.net>

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import CustomButton from "../button/button.component";
 import "./collection-item.styles.scss";
 
 const CollectionItem = ({ id, name, price, imageUrl }) => (
@@ -13,6 +14,7 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
       <span className="name">{name}</span>
       <span className="price">${price}</span>
     </div>
+    <CustomButton inverted>Add to Cart</CustomButton>
   </div>
 );
 export default CollectionItem;
