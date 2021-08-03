a809ff38b6362573a3f2a6c7b03fc0d1475f748f
jerome amos <mail@amosdesigns.net>
2021-08-03 13:22:34 -0400

    Added cart to nav, and style
    Signed-off-by: jerome amos <mail@amosdesigns.net>

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,10 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import {connect} from 'react-redux';
+import { connect } from "react-redux";
 import { auth } from "../../firebase/firebase.utils";
 import "./header.styles.scss";
-import { ReactComponent as Logo } from "../../crown.svg";
+import { ReactComponent as Logo } from "../../assets/crown.svg";
+import CartIcon from "../cart-icon/cart-icon.component";
 
 const Header = ({ currentUser }) => (
   <header className="header">
@@ -22,14 +23,17 @@ const Header = ({ currentUser }) => (
           SIGN OUT
         </div>
       ) : (
-        <Link className="option" to="/signin">SIGN IN</Link>
+        <Link className="option" to="/signin">
+          SIGN IN
+        </Link>
       )}
+      <CartIcon />
     </nav>
   </header>
 );
 
-const mapStateToProps = (state) =>({
-  currentUser: state.user.currentUser
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
 });
 
-export default connect (mapStateToProps)(Header);
+export default connect(mapStateToProps)(Header);
