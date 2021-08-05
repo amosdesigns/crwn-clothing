5b7e3a6e46d5da9345a0d99fe3dfaab4cf5c23ca
jerome amos <mail@amosdesigns.net>
2021-08-05 19:26:20 -0400

    Added update to cart value display
    Signed-off-by: jerome amos <mail@amosdesigns.net>

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -3,15 +3,21 @@ import { connect } from "react-redux";
 import { toggleCartHidden } from "../../redux/cart/cart.actions";
 import "./cart-icon.styles.scss";
 
-const CartIcon = ({ toogleCartHidden }) => (
+const CartIcon = ({ toogleCartHidden, itemCount }) => (
   <div className="cart-Icon" onClick={toogleCartHidden}>
     <i className="fal fa-shopping-cart shopping-Icon"></i>
-    <span className="item-count">0</span>
+    <span className="item-count">{itemCount}</span>
   </div>
 );
 
 const mapDispatchToProps = (dispatch) => ({
   toogleCartHidden: () => dispatch(toggleCartHidden()),
 });
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  itemCount: cartItems.reduce(
+    (accumalatedQuantity, cartItem) => accumalatedQuantity + cartItem.quantity,
+    0
+  ),
+});
 
-export default connect(null, mapDispatchToProps)(CartIcon);
+export default connect(mapStateToProps, mapDispatchToProps)(CartIcon);
