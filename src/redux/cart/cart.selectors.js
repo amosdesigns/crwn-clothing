b5973c91e986dd2b6fbd7c82b1e1eca0e8c9180c
jerome amos <mail@amosdesigns.net>
2021-08-30 10:11:42 -0400

    addedthe two digit for pricing
    Signed-off-by: jerome amos <mail@amosdesigns.net>

--- src/redux/cart/cart.selectors.js
+++ src/redux/cart/cart.selectors.js
@@ -25,7 +25,7 @@ export const selectCartItemsCount = createSelector(
 export const selectCartTotal = createSelector([selectCartItems], (cartItems) =>
   cartItems.reduce(
     (accumalatedQuantity, cartItem) =>
-      accumalatedQuantity + cartItem.quantity * cartItem.price,
+      accumalatedQuantity + cartItem.quantity * cartItem.price.toFixed(2),
     0
   )
 );
